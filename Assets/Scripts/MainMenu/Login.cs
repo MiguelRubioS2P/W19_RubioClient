f76fb4ed941823051c14595e2243c240e9aa7f55
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2020-02-11 16:35:55 +0100

    login y logout funcionales

--- Assets/Scripts/MainMenu/Login.cs
+++ Assets/Scripts/MainMenu/Login.cs
@@ -19,34 +19,94 @@ public class Login : MonoBehaviour
 
     private void Start()
     {
+        playerManager = FindObjectOfType<Player>();
+
         httpServerAddress = playerManager.GetHttpServer();
     }
 
     public void OnLoginButtonClicked()
     {
-
+        StartCoroutine(TryLogin());
     }
 
     private void GetToken()
     {
+        UnityWebRequest httpClient = new UnityWebRequest(httpServerAddress + "/Token", "POST");
+
+        WWWForm dataToSend = new WWWForm();
+        dataToSend.AddField("grant_type", "password");
+        dataToSend.AddField("username", emailInputField.text);
+        dataToSend.AddField("password", passwordInputField.text);
+
+        httpClient.uploadHandler = new UploadHandlerRaw(dataToSend.data);
+        httpClient.downloadHandler = new DownloadHandlerBuffer();
+
+        httpClient.SetRequestHeader("Accept", "application/json");
+        httpClient.SendWebRequest();
+
+        while (!httpClient.isDone)
+        {
+            Task.Delay(10);
+        }
+
+        if(httpClient.isNetworkError || httpClient.isHttpError)
+        {
+            Debug.Log(httpClient.error);
+        }
+        else
+        {
+            string jsonResponse = httpClient.downloadHandler.text;
+            AuthorizationToken authToken = JsonUtility.FromJson<AuthorizationToken>(jsonResponse);
+            playerManager.Token = authToken.access_token;
+        }
+
+        httpClient.Dispose();
 
     }
 
-    private void TryLogin()
+    private IEnumerator TryLogin()
     {
+        if (string.IsNullOrEmpty(playerManager.Token))
+        {
+            GetToken();
+        }
+
+        UnityWebRequest httpClient = new UnityWebRequest(httpServerAddress + "api/Account/UserId", "GET");
+
+        httpClient.SetRequestHeader("Authorization", "bearer " + playerManager.Token);
+        httpClient.SetRequestHeader("Accept", "application/json");
+
+        httpClient.downloadHandler = new DownloadHandlerBuffer();
+
+        yield return httpClient.SendWebRequest();
+
+        if(httpClient.isNetworkError || httpClient.isHttpError)
+        {
+            Debug.Log(httpClient.error);
+        }
+        else
+        {
+            playerManager.PlayerId = httpClient.downloadHandler.text;
+            messageBoardText.text += "\nWellcome " + playerManager.PlayerId + ". Yue are logged in!";
+            loginButton.interactable = false;
+            logoutButton.interactable = true;
+            playGameButton.interactable = true;
+        }
 
     }
 
     public void OnLogoutButtonClicked()
     {
-        TryLogout();
+        StartCoroutine(TryLogout());
     }
 
-    private void TryLogout()
+    private IEnumerator TryLogout()
     {
-        UnityWebRequest httpClient = new UnityWebRequest();
+        UnityWebRequest httpClient = new UnityWebRequest(httpServerAddress + "api/Account/Logout","POST");
 
-        httpClient.SetRequestHeader("", "" + playerManager.Token);
+        httpClient.SetRequestHeader("Authorization", "bearer " + playerManager.Token);
+
+        yield return httpClient.SendWebRequest();
 
         if (httpClient.isNetworkError || httpClient.isHttpError)
         {
